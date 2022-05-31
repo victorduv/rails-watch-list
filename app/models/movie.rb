cf531126605957ac657579c155c4b2f7fdbd4c63
Victor Duverne <victor.duverne@gmail.com>
2022-05-31 12:59:22 +0800

    chal1

--- app/models/movie.rb
+++
@@ -1,5 +0,0 @@
-class Movie < ApplicationRecord
-  has_many :bookmarks
-  validates :title, presence: true, uniqueness: true, allow_blank: false
-  validates :overview, presence: true, allow_blank: false
-end
